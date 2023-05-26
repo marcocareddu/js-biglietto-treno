6def59fbf6a4ba912e95d0448dd31286e0cb731f
marcocareddu <saldumaccu@tuta.io>
2023-05-26 14:58:25 +0200

    Var declared

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,20 @@
 console.log('JS OK')
+
+// FASE PRELIMINARE
+
+// Punto all'elemento della pagina
+const paragraph = document.getElementById('display-result');
+
+// Creo le Variabili costanti che serviranno per il calcolo successivo
+const under18 = 18;
+const over65 = 65;
+const discount40 = 40;
+const discount20 = 20;
+const costo_km = 0.21;
+
+// Verifico le variabili in console
+console.log(under18);
+console.log(over65);
+console.log(discount40);
+console.log(discount20);
+console.log(costo_km);
