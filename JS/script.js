5e26d81f6f7c5ffde1e029be5f06d1d9bc4c70ea
marcocareddu <saldumaccu@tuta.io>
2023-05-26 15:29:31 +0200

    added age/discount calc

--- JS/script.js
+++ JS/script.js
@@ -8,15 +8,13 @@ const paragraph = document.getElementById('display-result');
 // Creo le Variabili costanti che serviranno per il calcolo successivo
 const under18 = 18;
 const over65 = 65;
-const discount40 = 40;
-const discount20 = 20;
+let discount;
 const costo_km = 0.21;
 
 // Verifico le variabili in console
 console.log(under18);
 console.log(over65);
-console.log(discount40);
-console.log(discount20);
+console.log(discount);
 console.log(costo_km);
 
 
@@ -27,3 +25,19 @@ console.log(km_amount);
 
 const age = parseInt(prompt('Qual è la tua età?'));
 console.log(age);
+
+// Verifico se l'utente è maggiorenne
+if ((age >= under18) && (age <= over65)) {
+    discount = 0;
+    console.log("Non hai diritto allo sconto");
+
+} else if (age <= under18) {
+    discount = 20;
+    console.log("Sei ggggiovane, hai uno sconto del " + discount + "%!");
+
+}     else {
+    discount = 40;
+    console.log("Sei vecio!, hai uno sconto del " + discount + "%!");
+}
+
+console.log(discount);
