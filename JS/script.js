141d5e826b3d1440cad4500bed134a595cc69271
marcocareddu <saldumaccu@tuta.io>
2023-06-15 18:28:01 +0200

    JS - Removed console.log

--- JS/script.js
+++ JS/script.js
@@ -18,21 +18,10 @@ const over65 = 64;
 let discount;
 const cost_km = 0.21;
 
-// Verifico le variabili in console
-console.log(under18);
-console.log(over65);
-console.log(discount);
-console.log(cost_km);
-
-
 // SVOLGIMENTO
 // Chiedo i dati all'utente con parsInt e li salvo in variabile
 const km_amount = parseInt(prompt('Ciao! Quanti km intendi fare?'));
-console.log(km_amount);
-
 const age = parseInt(prompt('Qual è la tua età?'));
-console.log(age);
-
 
 // ! VALIDAZIONE
 // Dichiaro una variabile che cambierà a seconda dell'input dell'utente
@@ -50,7 +39,6 @@ if (isCorrect) {
     if ((age >= under18) && (age <= over65)) {
         discount = 0;
         console.log("Non hai diritto allo sconto");
-        console.log(discount);
 
     } else if (age <= under18) {
         discount = 20;
@@ -61,11 +49,6 @@ if (isCorrect) {
         console.log("Sei vecio!, hai uno sconto del " + discount + "%!");
     }
 
-
-    // Verifico che valore ha assunto la variabile discount
-    console.log(discount);
-
-
     // Calcolo il costo del biglietto e lo stampo in console con 2 numeri dopo la virgola
     let ticket = (km_amount * cost_km);
     console.log(ticket.toFixed(2));
@@ -73,19 +56,16 @@ if (isCorrect) {
     // Dichiaro la variabile che poi assumerà valore nell'istruzione condizionale
     let ticket_net;
 
-
     // Calcolo il costo del biglietto scorporando lo sconto
     if (discount === 0) {
         console.log("Il prezzo del biglietto è di €" + ticket.toFixed(2));
         ticket_net = ticket;
 
     } else if (discount === 20) {
-        console.log(ticket);
         ticket_net = ticket - ((ticket * discount) / 100);
         console.log("Il prezzo del biglietto è di €" + ticket_net.toFixed(2));
 
     } else {
-        console.log(ticket);
         ticket_net = ticket - ((ticket * discount) / 100);
         console.log("Il prezzo del biglietto è di €" + ticket_net.toFixed(2));
     }
